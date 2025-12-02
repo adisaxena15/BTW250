8999c7cc7b1ec0f4e6e8bc7188187f8bc4292fbe
Aditya Saxena <saxena11@illinois.edu>
2025-12-02 00:09:24 -0600

    deploy test

---
+++ scripts/rename-out-to-docs.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const outDir = path.join(__dirname, '..', 'out');
+const docsDir = path.join(__dirname, '..', 'docs');
+
+// Remove existing docs directory if it exists
+if (fs.existsSync(docsDir)) {
+  fs.rmSync(docsDir, { recursive: true, force: true });
+  console.log('✓ Removed existing docs directory');
+}
+
+// Rename out to docs
+if (fs.existsSync(outDir)) {
+  fs.renameSync(outDir, docsDir);
+  console.log('✓ Renamed out directory to docs');
+} else {
+  console.error('✗ Error: out directory does not exist');
+  process.exit(1);
+}
+
